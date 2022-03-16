9886d552d49ca5e032d5b969c8a658b5d1ed7832
candy <C110196104@nkust.edu.tw>
2022-03-16 17:10:25 +0800

    加入專案檔案。

---
+++ 220316_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _220316_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int stars;
+            Console.Write("Stars:");
+            stars = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < stars; i++)
+            {
+                Console.Write("★");
+            }
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+    }
+}
